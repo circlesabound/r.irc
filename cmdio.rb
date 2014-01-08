691e5f2f6d7e4a702656e423c7c54b5af1897bfd
mrmozila <deliciouslytesting@gmail.com>
2014-01-08 12:53:50 +1100

    combined io

---
+++ cmdio.rb
@@ -0,0 +1,49 @@
+require 'socket'
+require 'observer'
+require 'rubygems'
+
+puts "r.irc v0.1a"
+print "Enter server location: "
+server = gets.chomp
+print "Enter port (default 6667): "
+port = gets.chomp
+port = (port.length==0) ? "6667" : port
+
+puts "Connecting to #{server}:#{port} ..."
+
+s = TCPSocket.new server,port
+
+puts "Connection successful"
+print "User name: "
+username=gets.chomp
+print "Real name: "
+realname = gets.chomp
+user = username + " 0 * " + realname
+s.puts "USER " + user
+print "Nickname: "
+nickname = gets.chomp
+s.puts "NICK " + nickname
+
+print "Enter channel name (begins with \# or \$): "
+channel = gets.chomp
+s.puts "JOIN #{channel}"
+
+threads = []
+threads << Thread.new do
+	while incoming = s.gets
+		puts incoming
+	end
+end
+threads << Thread.new do
+	loop {
+		input = gets.chomp
+		if input==("/quit"||"/exit"||"/bye")
+			break
+		else
+			s.puts "PRIVMSG #{channel} #{input}"
+		end
+	}
+	threads.each {|thr| thr.kill}
+end
+
+threads.each {|thr| thr.join}
